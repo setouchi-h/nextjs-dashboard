aa50c234da6740c7d0e543bdcb6c8684e1f7a2ab
setouchi-h <setouchi.kazuki@gmail.com>
2023-11-15 18:10:34 +0900

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
-
 import Link from "next/link";
 import { lusitana } from "./ui/fonts";
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
